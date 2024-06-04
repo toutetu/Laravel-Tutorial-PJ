53d8ee31e2539e1042ccad22ce09cd5ba4fbcb4d
Tomoko Takahashi <toutetu144@gmail.com>
2024-06-04 20:25:02 +0900

    first

---
+++ src/Laravel9TaskList/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/* TaskControllerクラスを名前空間でインポートする */
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+
+/* index page */
+Route::get("/folders/{id}/tasks", [TaskController::class,"index"])->name("tasks.index");
+});
