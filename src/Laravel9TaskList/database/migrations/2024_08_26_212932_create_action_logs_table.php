8c3c7bc749914856213044c53f78f837c39d3ac5
toutetu <toutetu144@gmail.com>
2024-08-26 22:32:17 +0900

    env戻す

---
+++ src/Laravel9TaskList/database/migrations/2024_08_26_212932_create_action_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('action_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('screen'); // 画面名やURLを保存
+            $table->string('action'); // 実行されたアクション
+            // $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('action_logs');
+    }
+};
